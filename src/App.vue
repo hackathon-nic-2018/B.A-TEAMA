103af0eb03020e471fd354164fe20ebf123a73dc
HelAmpie <ampiehelen@gmail.com>
2018-11-29 14:18:20 -0600

    Agregando menu al layout principal

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -6,4 +6,61 @@
     </div>
     <router-view/>
   </div>
+</template>-->
+<template>
+  <v-layout row>
+    <v-flex xs12>
+      <v-card>
+        <v-card-title class="blue white--text">
+          <span class="headline">Menu</span>
+
+          <v-spacer></v-spacer>
+          <div>
+            <v-btn color="success">Success</v-btn>
+            <v-menu offset-y>
+      <v-btn
+        slot="activator"
+        color="primary"
+        dark
+      >
+        Dropdown
+      </v-btn>
+      <v-list>
+        <v-list-tile
+          v-for="(item, index) in items"
+          :key="index"
+          @click=""
+        >
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+          </div>
+          
+          
+        </v-card-title>
+
+        <router-view/>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' }
+      ]
+    })
+  }
+</script>
+
+<style>
+.success{
+  background-color: black !important;
+}
+</style>
