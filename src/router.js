229b845d4684783f13259c5eb9a90dd84815c586
HelAmpie <ampiehelen@gmail.com>
2018-11-29 22:35:24 -0600

    Maquetando vista Perfil

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Perfil from './views/Perfil.vue'
+import Buscador from './views/Buscador.vue'
 
 Vue.use(Router)
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/perfil',
+      name: 'perfil',
+      component: Perfil
+    },
+    {
+      path: '/buscar',
+      name: 'buscar',
+      component: Buscador
     },
     {
       path: '/about',
---
+++ src/views/Perfil.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <ul class="list-unstyled">
+            <b-media tag="li">
+                <img slot="aside" blank blank-color="#abc" width="90" src="https://firebasestorage.googleapis.com/v0/b/hacknic2018.appspot.com/o/img%2Fuser.png?alt=media&token=a9d59d0f-4084-4437-9354-ac81196391f9" alt="">
+                <h5 class="mt-0 mb-1">Juan López López</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                nte sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus
+                viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec
+                lacinia congue felis in faucibus.
+            </b-media>
+        </ul>
+      </div>
+      <h3>PRODUCTOS</h3>
+      <div class="container text-center"> <!--seccion card projectos-->
+        <div class="row">
+            <div class="col-sm">
+                <b-card title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2">
+                    <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            <div class="col-sm">
+                <b-card title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2">
+                    <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            <div class="col-sm">
+                <b-card title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2">
+                    <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+        </div>
+    </div> 
+    <h3>CLIENTES INTERESADOS</h3> 
+  </div>
+</template>
+<script>
+
+</script>
+<style>
+.list-unstyled{
+    padding: 30px;
+}   
+</style>
+
