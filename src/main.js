212eed3db9fb072b27eb46d0e0b59d9828a5c634
José Daniel Narváez Flores <jdnarvaezf@gmail.com>
2018-11-30 03:38:30 -0600

    setting up vue-resource

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueResource from 'vue-resource'
 
 Vue.use(BootstrapVue)
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
 new Vue({
